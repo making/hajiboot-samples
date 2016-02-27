ef131ffe48bb2fbb29a8814da28d547974fd9021
making <makingx@gmail.com>
2016-02-28 01:04:03 +0900

    Update 2.2.4.2

--- chapter02/2.2.4.2_hajiboot-jdbc/src/main/java/com/example/App.java
+++ chapter02/2.2.4.2_hajiboot-jdbc/src/main/java/com/example/App.java
@@ -5,11 +5,16 @@ import com.example.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
-@EnableAutoConfiguration
-@ComponentScan
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@SpringBootApplication
 public class App implements CommandLineRunner {
     @Autowired
     CustomerRepository customerRepository;
--- chapter02/2.2.4.2_hajiboot-jdbc/src/main/java/com/example/AppConfig.java
+++ chapter02/2.2.4.2_hajiboot-jdbc/src/main/java/com/example/AppConfig.java
@@ -2,28 +2,35 @@ package com.example;
 
 import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
     @Autowired
-    DataSourceProperties dataSourceProperties; // (1)
+    DataSourceProperties dataSourceProperties;
     DataSource dataSource;
 
-    @Bean
+    @Bean(destroyMethod = "close")
+    @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
     DataSource realDataSource() {
-        // (2)
         DataSourceBuilder factory = DataSourceBuilder
-                .create(this.dataSourceProperties.getClassLoader()).url(this.dataSourceProperties.getUrl()).username(this.dataSourceProperties.getUsername()).password(this.dataSourceProperties.getPassword());
+                .create(this.dataSourceProperties.getClassLoader())
+                .url(this.dataSourceProperties.getUrl())
+                .username(this.dataSourceProperties.getUsername())
+                .password(this.dataSourceProperties.getPassword());
         this.dataSource = factory.build();
         return this.dataSource;
     }
 
+    @Primary
     @Bean
     DataSource dataSource() {
         return new Log4jdbcProxyDataSource(this.dataSource);
