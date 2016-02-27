fc237066330f1eab11b7226dc1c5a2841ac8a3c2
making <makingx@gmail.com>
2016-02-28 00:22:06 +0900

    Update 2.1.4

--- chapter02/2.1.4_hajiboot-di/src/main/java/com/example/App.java
+++ chapter02/2.1.4_hajiboot-di/src/main/java/com/example/App.java
@@ -2,13 +2,10 @@ package com.example;
 
 import com.example.app.Frontend;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
