c375eb08bae243e1b386d5c0a2a68677ebaf1e0a
making <makingx@gmail.com>
2016-02-28 00:38:10 +0900

    Update 2.2.1.2

--- chapter02/2.2.1.2_hajiboot-jdbc/src/main/java/com/example/App.java
+++ chapter02/2.2.1.2_hajiboot-jdbc/src/main/java/com/example/App.java
@@ -3,23 +3,22 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class App implements CommandLineRunner {
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT :a + :b"; // (1)
+        String sql = "SELECT :a + :b";
         SqlParameterSource param = new MapSqlParameterSource()
                 .addValue("a", 100)
-                .addValue("b", 200); // (2)
+                .addValue("b", 200);
         Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
 
         System.out.println("result = " + result);
