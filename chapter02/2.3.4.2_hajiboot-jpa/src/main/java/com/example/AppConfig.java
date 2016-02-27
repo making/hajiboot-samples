598fbc1d29335edc6cd6035bd261f39d796e6ba8
making <makingx@gmail.com>
2016-02-28 01:23:59 +0900

    Update 2.3.4.2

--- chapter02/2.3.4.2_hajiboot-jpa/src/main/java/com/example/AppConfig.java
+++ chapter02/2.3.4.2_hajiboot-jpa/src/main/java/com/example/AppConfig.java
@@ -1,28 +1,36 @@
 package com.example;
 
 import net.sf.log4jdbc.Log4jdbcProxyDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
-    private DataSource dataSource;
+    @Autowired
+    DataSourceProperties dataSourceProperties;
+    DataSource dataSource;
 
     @Bean(destroyMethod = "close")
+    @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
     DataSource realDataSource() {
         DataSourceBuilder factory = DataSourceBuilder
-                .create()
-                .driverClassName("org.h2.Driver")
-                .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
-                .username("sa")
-                .password("");
+                .create(this.dataSourceProperties.getClassLoader())
+                .url(this.dataSourceProperties.getUrl())
+                .username(this.dataSourceProperties.getUsername())
+                .password(this.dataSourceProperties.getPassword());
         this.dataSource = factory.build();
-        return dataSource;
+        return this.dataSource;
     }
 
+    @Primary
     @Bean
     DataSource dataSource() {
         return new Log4jdbcProxyDataSource(this.dataSource);
