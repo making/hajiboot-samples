cb1883be1f3b394db9c4c2c944e9c36d3f3b2500
making <makingx@gmail.com>
2016-07-09 17:23:07 +0900

    Update 2.2.4.2 (bump Spring Boot to 1.4)

---
+++ chapter02/2.2.4.2_hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibootJdbcApplication implements CommandLineRunner {
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		// データ追加
+		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + " is created!");
+		// データ表示
+		customerRepository.findAll()
+				.forEach(System.out::println);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootJdbcApplication.class, args);
+	}
+}
