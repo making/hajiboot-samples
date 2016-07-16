12962873e03391cfaa7aa3d59079f859d3764eae
making <makingx@gmail.com>
2016-07-17 03:13:40 +0900

    Update 3.4(bump Spring Boot to 1.4)

--- chapter03/3.4_hajiboot-flyway/src/main/java/com/example/HajibootThymeleafApplication.java
+++ chapter03/3.4_hajiboot-flyway/src/main/java/com/example/HajibootThymeleafApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+public class HajibootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootThymeleafApplication.class, args);
+	}
 }
