e31783f39dd5e9ec0d4ede9f98230f74e589e78b
making <makingx@gmail.com>
2016-08-11 01:02:51 +0900

    Use ResponseEntity.created(location).body(created);

--- chapter03/3.2.2_hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ chapter03/3.2.2_hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -4,7 +4,6 @@ package com.example.api;
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,9 +35,7 @@ public class CustomerRestController {
         Customer created = customerService.create(customer);
         URI location = uriBuilder.path("api/customers/{id}")
                 .buildAndExpand(created.getId()).toUri();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(location);
-        return new ResponseEntity<>(created, headers, HttpStatus.CREATED);
+        return ResponseEntity.created(location).body(created);
     }
 
     @PutMapping(path = "{id}")
