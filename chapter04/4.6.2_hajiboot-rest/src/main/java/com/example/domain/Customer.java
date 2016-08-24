a8358d2421a13bf66e1215ba4ae9894f9e786df8
making <makingx@gmail.com>
2016-08-24 10:51:19 +0900

    Update Chapter4

---
+++ chapter04/4.6.2_hajiboot-rest/src/main/java/com/example/domain/Customer.java
@@ -0,0 +1,24 @@
+
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+}
---
+++ chapter04/4.7_hajiboot-rest/src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,42 @@
+package com.example.service;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll() {
+        return customerRepository.findAllOrderByName();
+    }
+
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAllOrderByName(pageable);
+    }
+
+    public Customer findOne(Integer id) {
+        return customerRepository.findOne(id);
+    }
+
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public void delete(Integer id) {
+        customerRepository.delete(id);
+    }
+}
